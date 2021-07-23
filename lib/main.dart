396e2aba7fc66d9329e535e2c442f14a64f1702b
ALVI0017 <alvi0019@gmail.com>
2021-07-24 01:02:46 +0600

    Some minor bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:ecommerce_app/data/repository/trending_prodct_repository.dart';
+import 'package:ecommerce_app/ui/screens/home/components/body.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/routes.dart';
 import 'package:ecommerce_app/ui/screens/splash/splash_screen.dart';
 import 'package:ecommerce_app/ui/theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/bloc.dart';
+import 'data/repository/new_arrivals_repository.dart';
+import 'data/repository/new_shops_repository.dart';
+import 'data/repository/products_repository.dart';
+import 'data/repository/trending_seller_repository.dart';
 
 void main() {
   runApp(MyApp());
